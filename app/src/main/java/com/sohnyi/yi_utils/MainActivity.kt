af5bf1c7fc252d87a9999da0f7a32512e53c605d
sohnyi <sohnyi@163.com>
2021-05-02 02:05:28 +0800

    调整 'app' 展示内容

--- app/src/main/java/com/sohnyi/yi_utils/MainActivity.kt
+++ app/src/main/java/com/sohnyi/yi_utils/MainActivity.kt
@@ -1,32 +1,19 @@
 package com.sohnyi.yi_utils
 
-import androidx.appcompat.app.AppCompatActivity
+import android.annotation.SuppressLint
 import android.os.Bundle
-import android.util.Log
-import com.sohnyi.timeuti.apartToday
-import com.sohnyi.timeuti.isToady
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.sohnyi.timeuti.toDateString
 import java.util.*
 
-private const val TAG = "MainActivity"
 class MainActivity : AppCompatActivity() {
 
 
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val time: Long = 1619772659442L
-        Log.d(TAG, "onCreate: ${time.isToady}");
-
-        val c1 = Calendar.getInstance()
-        c1.set(Calendar.YEAR, 2021);
-        c1.set(Calendar.MONTH, 4);
-        c1.set(Calendar.DAY_OF_MONTH, 1);
-
-        val c2 = Calendar.getInstance();
-
-        Log.d(TAG, "onCreate: ${c1.apartToday}")
-
-
+        findViewById<TextView>(R.id.text_view).text = "${getString(R.string.hi)} ${Date().toDateString("yyyy-MM-dd")}"
     }
 }
