3778e065e0f5d7584bf4514ec018d11dea7473fd
sohnyi <sohnyi@163.com>
2021-05-02 02:01:05 +0800

    Add the 'DateEtx.kt'

---
+++ timeutil/src/main/java/com/sohnyi/timeuti/DateEtx.kt
@@ -0,0 +1,37 @@
+package com.sohnyi.timeuti
+
+import java.text.SimpleDateFormat
+import java.util.*
+
+/**
+ * Date 拓展类
+ * Create by yi on 5/2/21
+ */
+
+private const val DEFAULT_FORMAT_PATTERN: String = "yyyy/MM/dd HH:mm:ss"
+
+/**
+ * 日期格式化
+ */
+fun Date.toDateString(
+    pattern: String = DEFAULT_FORMAT_PATTERN,
+    locale: Locale = Locale.getDefault()
+): String {
+    val format = SimpleDateFormat(pattern, locale)
+    return format.format(this)
+}
+
+/**
+ * 是否为今天
+ */
+val Date.isToady: Boolean
+    get() {
+        return if (this == Date()) {
+            true
+        } else {
+            Calendar.getInstance().apply {
+                time = this@isToady
+            }.isToday
+        }
+    }
+
